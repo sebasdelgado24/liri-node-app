35639f94de8918159a102096d19a3c91706c4b6b
Juan Sebastian Delgado Rincon <juansebastiandelgadorincon@Juans-MacBook-Pro.local>
2019-05-01 02:48:33 -0500

    liri.js code is returning an object when running the application

--- liri.js
+++ liri.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 
 const keys = require("./keys.js");
 
+var Spotify = require('node-spotify-api');
+
 const spotify = new Spotify(keys.spotify);
 
-//spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-   // if (err) {
-     // return console.log('Error occurred: ' + err);
-   // }
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
    
-  //console.log(data); 
-  //});
+  console.log(data); 
+  });
+
+
+
+
